5aee7889e43d9f35cc3cae24c0b00e30b7627710
Arthur Barbosa <arthurbruno03@gmail.com>
2019-08-19 12:24:13 -0400

    interceptor

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -29,9 +29,7 @@ export class CategoriasPage {
       response => {
         this.items = response;
       },
-      error => {
-        console.log(error);
-      }
+      error => {}
     );
   }
 }
