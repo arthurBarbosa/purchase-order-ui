9a1d84e08febc434ff4187b9c98bc7d2c52f375e
Arthur Barbosa <arthurbruno03@gmail.com>
2019-09-18 10:18:09 -0400

    infinite scroll 100% e acesso a camera 40%

--- src/pages/profile/profile.module.ts
+++ src/pages/profile/profile.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { ProfilePage } from './profile';
-
+import { NgModule } from "@angular/core";
+import { IonicPageModule } from "ionic-angular";
+import { ProfilePage } from "./profile";
+import { Camera, CameraOriginal } from "@ionic-native/camera";
 @NgModule({
-  declarations: [
-    ProfilePage,
-  ],
-  imports: [
-    IonicPageModule.forChild(ProfilePage),
-  ],
+  declarations: [ProfilePage],
+  imports: [IonicPageModule.forChild(ProfilePage)],
+  providers: [CameraOriginal]
 })
 export class ProfilePageModule {}
